9361fa7590cddfd18fc135df53a0b2bdc59e6c31
Erin Wiegman <erin@bluecadet.com>
2023-03-30 19:31:55 -0400

    sample structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Album from './components/Album.js';
 
 function App() {
+  const sampleData = [
+    {
+      id: 'recxHvVai51GUBmd1',
+      album: 'album title',
+      year: '2023',
+      link: 'bandcamp.com/song',
+    },
+    {
+      id: 'recxHvVai51GUBmd2',
+      album: 'album title',
+      year: '2022',
+      link: 'bandcamp.com/song',
+    },
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>North Room Records</p>
+        {sampleData.map((album, index) => (
+          <Album key={index} album={album} />
+        ))}
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/Album.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+const Album = ({ album }) => {
+  return <div className="year">{album.year}</div>;
+};
+
+export default Album;
